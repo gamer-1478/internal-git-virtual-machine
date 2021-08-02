9cd7e48e9c19019bb788c15cecb33558e0e381a1
gamer-12748 <aayushgarg2006@gmail.com>
2021-08-02 23:43:16 +0530

    oops

--- gitolite/push.py
+++ gitolite/push.py
@@ -3,8 +3,8 @@ from subprocess import Popen
 
 import time
 
-subprocess.Popen("git add .", cwd="/home/tsadmin/gitolite-admin")
+subprocess.Popen("git add .", cwd="/home/tsadmin/gitolite-admin/", shell=True)
 time.sleep(1)
-subprocess.Popen('git commit -m "added some data"', cwd="/home/tsadmin/gitolite-admin")
+subprocess.Popen('git commit -m "added some data"', cwd="/home/tsadmin/gitolite-admin/", shell=True)
 time.sleep(2)
-subprocess.Popen("git push origin main", cwd="/home/tsadmin/gitolite-admin")
+subprocess.Popen("git push origin main", cwd="/home/tsadmin/gitolite-admin/", shell=True)
