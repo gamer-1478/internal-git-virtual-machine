8c5f167f924fb36749265989ec99f5f2c4dc5910
gamer-12748 <aayushgarg2006@gmail.com>
2021-08-08 20:10:35 +0530

    vm

--- models/deploy.py
+++ models/deploy.py
@@ -59,6 +59,7 @@ if os.path.exists('/home/tsadmin/deploys/'+username+'/'+node_repo):
     subprocess.Popen("npm install", cwd="/home/tsadmin/deploys/"+username+'/'+node_repo, shell=True, stdout=logs, stderr=logs)
     time.sleep(10)
     c = subprocess.Popen("npm install", cwd="/home/tsadmin/deploys/"+username+'/'+node_repo, shell=True, stdout=logs, stderr=logs)
+    time.sleep(3)
     print(c.poll())
     while c.poll() != None:
         node = subprocess.Popen('PORT='+port+" " + "npm start", cwd="/home/tsadmin/deploys/"+username+'/'+node_repo, shell=True, stdout=logs, stderr=logs)
@@ -79,6 +80,8 @@ else:
         subprocess.Popen("git checkout "+checkout, cwd="/home/tsadmin/deploys/"+username+'/'+node_repo, shell=True, stdout=logs, stderr=logs)
         time.sleep(3)
     c = subprocess.Popen("npm install", cwd="/home/tsadmin/deploys/"+username+'/'+node_repo, shell=True, stdout=logs, stderr=logs)
+    time.sleep(3)
+
     print(c.poll())
     while c.poll() != None:
         node = subprocess.Popen('PORT='+port+" " + "npm start", cwd="/home/tsadmin/deploys/"+username+'/'+node_repo, shell=True, stdout=logs, stderr=logs)
